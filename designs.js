438e53e5391d37ab50b3a3d59cd5c0a736045a1c
JohPik <38173353+JohPik@users.noreply.github.com>
2018-04-14 17:57:17 +1000

    Solve the Problem :)

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
-// Select color input
-// Select size input
+const form = document.querySelector('form');
+const grid = document.querySelector('table');
 
-// When size is submitted by the user, call makeGrid()
+// select size input
 
-function makeGrid() {
+function makeGrid(){
 
-// Your code goes here!
+  // delete previous grid before starting new grid
+  grid.innerHTML ='';
 
+  // set Grid values
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWidth").value;
+
+  // create Grid
+  for (let i = 1; i <= height; i++) {
+    let row = document.createElement('tr');
+      for (let u = 1; u <= width; u++) {
+        let cell = document.createElement('td');
+        row.appendChild(cell);
+      }
+      grid.appendChild(row);
+  }
 }
+
+form.addEventListener("submit", function(event) {
+  makeGrid();
+  event.preventDefault();
+});
+
+// Change color of Cells when clicked
+document.querySelector('table').addEventListener('click', function (evt) {
+    if (evt.target.nodeName === 'TD') {  // ← verifies target is desired element
+        let colorChosen = document.getElementById("colorPicker").value;
+          evt.target.style.backgroundColor = colorChosen;
+    }
+});
